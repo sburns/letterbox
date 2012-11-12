ed125e74420896827cab367921e758cd159f2754
Brian Dailey <brian@stratasan.com>
2012-11-12 15:49:06 -0600

    Removing test stubs and adding some tests to check permissions.

--- letterbox/tests.py
+++ letterbox/tests.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from django.test.client import Client
 from django.contrib.auth.models import User
 
 from letterbox.models import Notice
@@ -35,7 +34,7 @@ class NoticeTest(TestCase):
         notice = Notice.send_notice(self.jane, self.joe, "Test Message","Test Message")
         notice.archive()
         self.assertEqual(len(Notice.objects.archived(self.joe)), 1)
-        self.assertEqual(len(Notice.objects.notices_for(self.joe)), 0)
+        self.assertEqual(len(Notice.objects.notices_for(self.joe)), 1)
 
 
 
@@ -43,27 +42,20 @@ class ViewTests(TestCase):
     def setUp(self):
         self.joe = User.objects.create_user(username='joe',email='joe@acme.com',password='test')
         self.jane = User.objects.create(username='jane', email='jane@acme.com', password='test')
-        self.notice = Notice.objects.create(sender=self.joe, recipient=self.jane, subject='Test', message='Test Message')
+        self.jack = User.objects.create(username='jack', email='jack@acme.com', password='test')
+        self.notice = Notice.objects.create(recipient=self.joe, sender=self.jane, subject='Test', message='Test Message')
         self.client.login(username='joe', password='test')
 
     def test_default(self):
         response = self.client.get('/notices/')
         self.assertIn("notices", response.context)
         self.assertEqual(len(response.context['notices']), 1)
-        assert False
-
-    def test_archived(self):
-        assert False
-
-    def test_all(self):
-        assert False
-
-    def test_unread(self):
-        assert False
-
-    def test_archive(self):
-        assert False
 
     def test_detail(self):
         response = self.client.get('/notices/1/')
-        pass
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_permissions(self):
+        Notice.objects.create(recipient=self.jack, sender=self.jane, subject='Test', message='Test Message')
+        response = self.client.get('/notices/2/')
+        self.assertEqual(response.status_code, 404)
